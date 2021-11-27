e253882314ac0ebf76d13836171709933e0a3a14
mulekick <karim_laouari@hotmail.com>
2021-11-27 14:37:15 +0100

    move prompt for command history symlink

--- shell_update.sh
+++ shell_update.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# create symlink to command archive directory
-echo -e "$DASH_LINE\nplease provide your command archive directory path :"
-
-# read input from user
-read -r cmdarchdir
-
-# if directory does not exist, create
-if [[ ! -d $cmdarchdir ]]; then
-    mkdir -pv "$cmdarchdir"
-fi
-
-# create symlink in shell-extend directory
-ln -s "$cmdarchdir" "$HOME/.shell_extend/.cmd_archive"
-
 # set shell extension
 # shellcheck disable=SC2016
 extendshell='\n
